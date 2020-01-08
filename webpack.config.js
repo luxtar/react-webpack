42af97511382b9a5f5a3dec48e46fc04a8cce242
luxtar <luxtar21@gmail.com>
2020-01-09 03:33:50 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,44 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: "/node_modules",
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                  {
-                    loader: 'style-loader'
-                  },
-                  {
-                    loader: 'css-loader'
-                  }
-                ]
-              }
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: "/node_modules",
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
         ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-        // favicon: 'public/favicon.ico' 파비콘은 준비가 되어있지 않아 주석처리한다.
-    })],
-    devServer: {
-        historyApiFallback: true,
-        open: true,
-    },
-    externals: {
-        // global app config object
-        config: JSON.stringify({
-            apiUrl: 'http://localhost:8080'
-        })
-    },
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+      // favicon: 'public/favicon.ico' 파비콘은 준비가 되어있지 않으므로 주석처리
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+    // open: true,
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: "http://localhost:8080"
+    })
+  }
 }
